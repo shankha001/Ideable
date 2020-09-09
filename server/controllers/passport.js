92260dd8a9fd1f42f5be15317e62858bca6bf3a4
shankha001 <shankhasubhrabiswas@gmail.com>
2020-09-09 11:32:40 +0530

    [Passport] config passport

---
+++ server/controllers/passport.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const JwtStratergy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/User');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET_KEY;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStratergy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id).then((user) => {
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+        // or you could create a new account
+      });
+    })
+  );
+};
--- server/server.js
+++ server/server.js
@@ -1,20 +1,36 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const passport = require('passport');
 
 const PORT = process.env.PORT || 5000;
-const users = require('./routes/auth/users');
+
+//===MongoDB===/
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'));
 
 //===MIDDLEWARE===//
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(cors());
+
+//===PASSPORT===//
+app.use(passport.initialize());
+require('./controllers/passport')(passport);
 
 //===ROUTES===//
 app.get('/', (req, res) => {
   res.send({ msg: 'Welcome to the Backend Server' });
 });
+const users = require('./routes/auth/users');
 
 app.use('/auth/users', users);
 
