1000d7c73f949011e089e78f20b69d171447d4ad
shankha001 <shankhasubhrabiswas@gmail.com>
2020-09-09 14:17:05 +0530

    [Login] create and config login route

--- client/src/helper.js
+++ client/src/helper.js
@@ -6,3 +6,10 @@ export const register = (user) => {
     .then((res) => console.log(res))
     .catch((err) => console.log(err.response.data));
 };
+
+export const login = (user) => {
+  axios
+    .post('/auth/users/login', user)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err.response.data));
+};
